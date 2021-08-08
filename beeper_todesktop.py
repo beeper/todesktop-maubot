1767ff1e401264d9d2b639d9a40552376a08d68d
Tulir Asokan <tulir@maunium.net>
2021-08-08 12:35:56 +0300

    Add support for sending Android apk links

--- beeper_todesktop.py
+++ beeper_todesktop.py
@@ -1,4 +1,4 @@
-from typing import Type, Match, Optional
+from typing import Type, Match, Optional, Dict, Callable, Awaitable, Any
 import asyncio
 import secrets
 import json
@@ -23,18 +23,23 @@ class Config(BaseProxyConfig):
             helper.copy("webhook_secret")
         helper.copy("gitlab_url")
         helper.copy("gitlab_token")
-        helper.copy("build_name_map")
-        helper.copy("message_format")
+        helper.copy("projects")
 
 
 url_pattern = re.compile("https://dl.todesktop.com/([a-z0-9]+)/builds/([a-z0-9]+)")
+WebhookHandler = Callable[[Dict[str, Any], JSON], Awaitable[Dict[str, str]]]
 
 
 class TodesktopBot(Plugin):
     base_url: URL
+    webhook_handlers: Dict[str, WebhookHandler]
 
     async def start(self):
         self.on_external_config_update()
+        self.webhook_handlers = {
+            "todesktop": self.handle_todesktop,
+            "android": self.handle_android,
+        }
 
     @classmethod
     def get_config_class(cls) -> Type[BaseProxyConfig]:
@@ -45,16 +50,14 @@ class TodesktopBot(Plugin):
         self.base_url = URL(self.config["gitlab_url"])
 
     async def find_todesktop_build(self, project_id: int, job_id: int) -> Optional[Match]:
-        url = self.base_url / "api/v4/projects" / str(project_id) / "jobs" / str(job_id) / "trace"
+        url = self.base_url / f"api/v4/projects/{project_id}/jobs/{job_id}/trace"
         headers = {"PRIVATE-TOKEN": self.config["gitlab_token"]}
         async with ClientSession() as sess, sess.get(url, headers=headers) as resp:
             resp.raise_for_status()
             text = await resp.text()
         return url_pattern.search(text)
 
-    async def handle_webhook(self, room_id: RoomID, data: JSON) -> str:
-        if data["build_status"] != "success":
-            return "Non-success webhook ignored"
+    async def handle_todesktop(self, _: Dict[str, Any], data: JSON) -> Dict[str, str]:
         try:
             match = await self.find_todesktop_build(data["project_id"], data["build_id"])
         except Exception:
@@ -62,17 +65,34 @@ class TodesktopBot(Plugin):
             raise web.HTTPInternalServerError(text="500: Internal Server Error\n"
                                                    "Failed to get todesktop build ID\n")
         if not match:
-            return "Todesktop URL not found"
-        build_name = data["build_name"]
+            raise ValueError("Todesktop URL not found")
+        return {"todesktop_url": match.group(0)}
 
-        params = {
-            "build_name": self.config["build_name_map"].get(build_name, build_name),
+    @staticmethod
+    async def handle_android(project: Dict[str, Any], data: JSON) -> Dict[str, str]:
+        apk_path = project["apk_path_map"][data["build_name"]]
+        repo_url = data["repository"]["homepage"]
+        job_id = data["build_id"]
+        return {"apk_url": f"{repo_url}/-/jobs/{job_id}/artifacts/browse/{apk_path}"}
+
+    async def handle_webhook(self, room_id: RoomID, data: JSON) -> str:
+        try:
+            project = self.config["projects"][data["project_id"]]
+        except KeyError:
+            raise web.HTTPNotFound(text="404: Unknown project ID")
+        if data["build_status"] != "success":
+            return "Non-success webhook ignored"
+        try:
+            extra_params = await self.webhook_handlers[project["type"]](project, data)
+        except ValueError as e:
+            return str(e)
+        message = project["message_format"].format(**{
+            "build_name": project["build_name_map"][data["build_name"]],
             "commit_hash": data["sha"][:8],
             "commit_url": URL(data["repository"]["homepage"]) / "-" / "commit" / data["sha"],
-            "todesktop_url": match.group(0),
-        }
+            **extra_params,
+        })
         try:
-            message = self.config["message_format"].format(**params)
             await self.client.send_markdown(room_id, message, msgtype=MessageType.NOTICE)
         except Exception:
             self.log.exception("Error sending message to Matrix")
